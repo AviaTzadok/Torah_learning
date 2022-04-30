a7226666199f456d545f26aa1b2230386ccc41aa
AviaTzadok <73134926+AviaTzadok@users.noreply.github.com>
2022-04-30 22:08:45 +0300

    add bulletin

--- src/components/PlayingYouTubeVideo/PlayingYouTubeVideo.js
+++ src/components/PlayingYouTubeVideo/PlayingYouTubeVideo.js
@@ -7,8 +7,8 @@ const PlayingYouTubeVideo = ({ videoId }) => {
   return (
     <div className="video-responsive">
       <iframe
-        width="400"
-        height="300"
+        width="300"
+        height="240"
         src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
         frameBorder="0"
         allow="autoplay; accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
